bb2e9c272da57577e27ea7412376d8783cb09189
Tejan Singh <tstejan@gmail.com>
2022-10-02 16:46:09 +0530

    feat: added calculate change functionality

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const billInput = document.querySelector(".bill-input-field");
 const cashInput = document.querySelector(".cash-input-field");
 const waringMsg = document.querySelector("#warning");
 const checkBtn = document.querySelector(".btn");
+const noOfNotes = document.querySelectorAll('.no-of-notes')
+
+const availableNotes = [2000,500,200,100,50,20,10,5,2,1]
 
 const handleClick = () => {
   const billValue = Number(billInput.value);
   const cashValue = Number(cashInput.value);
-  const cashToReturn = billValue - cashValue
+  const cashToReturn = cashValue - billValue
 
   if (billValue > 0 && cashValue > 0) {
     if (billValue > cashValue) {
@@ -26,7 +29,12 @@ const handleClick = () => {
 };
 
 const calculateNotes = (cashToReturn)=>{
-    
+    for(var i = 0; i < availableNotes.length; i++){
+        const notes = Math.trunc(cashToReturn / availableNotes[i]) // to check for no. of notes for each available note
+        cashToReturn = cashToReturn % availableNotes[i] // update remaining amount
+
+        noOfNotes[i].innerHTML = notes
+    }
 }
 
 checkBtn.addEventListener("click", handleClick);
